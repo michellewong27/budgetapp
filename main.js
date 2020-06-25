0a025c55fd0de8b669ad538dd979bb3da8bc9b1e
Michelle Wong <michellekwong2@gmail.com>
2020-06-25 13:40:02 -0400

    show balance color values

--- main.js
+++ main.js
@@ -32,7 +32,29 @@ class UI {
                 //remove clas list to hide message
                 self.budgetFeedback.classList.remove('showItem');
             }, 4000);
+        } else {
+            this.budgetAmount.textContent = value;
+            this.budgetInput.value = '';
+            this.showBalance();
         }
+    }
+    
+    showBalance(){
+        const expense = this.totalExpense();
+        const total = parseInt(this.budgetAmount.textContent) - expense;
+        this.balanceAmount.textContent = total;
+        if(total < 0){
+            this.balance.style.color = 'red';
+        } else if (total > 0) {
+            this.balance.style.color = 'green';
+        } else if (total === 0) {
+            this.balance.style.color = 'black'
+        }
+    }
+
+    totalExpense(){
+        let total = 400;
+        return total;
     }
 }
 
